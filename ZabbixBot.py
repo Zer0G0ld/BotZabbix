4fbc9abd3b6c7e9188dc4f33eeff599a318a0778
Zer0G0ld <matheus321trabalho@gmail.com>
2023-12-29 17:35:42 -0300

    Ajustando e rodando comandos

--- ZabbixBot.py
+++ ZabbixBot.py
@@ -1,21 +1,21 @@
 import discord
 import requests
 import easysnmp
+import subprocess
 
 from discord.ext import commands
 from discord import Intents
 
 # Configurações do bot
 TOKEN = '<SEU_TOKEN_DO_DISCORD>'
-ZABBIX_API_TOKEN = '<SEU_TOKEN_DO_ZABBIX>'
-ZABBIX_API_BASE_URL = '<SUA_URL_DO_ZABBIX>'
+ZABBIX_API_TOKEN = 'SEU_TOKEN_DO_ZABBIX'
+ZABBIX_API_BASE_URL = 'SUA_URL_DO_ZABBIX'
 ZABBIX_API_URL = f'{ZABBIX_API_BASE_URL}?{ZABBIX_API_TOKEN}'
-ZABBIX_USERNAME = '<SEU_NOME_DE_USUARIO_ZABBIX>'
-ZABBIX_PASSWORD = '<SUA_SENHA_ZABBIX>'
+ZABBIX_USERNAME = 'SEU_NOME_DE_USUARIO_ZABBIX'
+ZABBIX_PASSWORD = 'SUA_SENHA_ZABBIX'
 
 intents = Intents.default()
 intents.messages = True
-intents.message_content = True
 description = "Um bot para auxiliar no monitoriamento do zabbix. \n\nQuero mostrar os incidentes, hosts e tudo mais, basta me chamar com o prefixo '!' junto do comando para eu te reponder. \n\nExemplo : '!incidentes'"
 
 
@@ -23,7 +23,7 @@ bot = commands.Bot(command_prefix="!", intents=intents, description=description)
 
 @bot.event
 async def on_ready():
-    print(f'Entrei! Me chamo {bot.user.name}')
+    print(f'Tô dentro! Me chamo {bot.user.name}')
 
 @bot.event
 async def on_message(message):
@@ -76,14 +76,44 @@ def get_zabbix_incidents():
     }
 
     incidents_response = requests.post(ZABBIX_API_BASE_URL, json=incidents_data).json()
-    incidents_list = [f"{incident['name']} ({incident['acknowledges'][0]['message']})" for incident in incidents_response.get('result', [])]
+    incidents_list = [f"{incident['name']} ({incident['acknowledges'][0]['message'] if incident['acknowledges'] else 'No acknowledges'})" for incident in incidents_response.get('result', [])]
+
 
     return '\n'.join(incidents_list)
 
+#@bot.command(name='snmpwalk')
+#async def snmpwalk(ctx, ip: str, oid: str):
+#    try:
+#       result = perform_snmpwalk(ip, oid)
+#       await ctx.send(f'SNMP Walk Result:\n{result}')
+#    except Exception as e:
+#        await ctx.send(f'Error during SNMP Walk:\n{str(e)}')
+
+#def perform_snmpwalk(ip, oid):
+#    try:
+#        session = easysnmp.Session(hostname=ip, community='public', version=2)
+#        result = session.walk(oid)
+#        return "\n".join([f'{var.oid}: {var.value}' for var in result])
+#    except Exception as e:
+#        raise Exception(f"Error in SNMP Walk: {str(e)}")
+
+#@bot.command(name='snmpwalk')
+#async def snmpwalk(ctx, ip: str, oid: str = '.1', *args):
+#    try:
+#        full_oid = oid + ' '.join(args)
+#        result = subprocess.check_output(['snmpwalk', '-v', '2c', '-c', 'public', ip, full_oid], text=True)
+#        await ctx.send(f'SNMP Walk Result:\n{result}')
+#    except subprocess.CalledProcessError as e:
+#        await ctx.send(f'Error during SNMP Walk:\n{e.output}')
+#    except Exception as e:
+#        await ctx.send(f'Error during SNMP Walk:\n{str(e)}')
+
+
 @bot.command(name='snmpwalk')
-async def snmpwalk(ctx, ip: str, oid: str):
+async def snmpwalk(ctx, ip: str, oid: str = '.1', *args):
     try:
-        result = perform_snmpwalk(ip, oid)
+        full_oid = oid + ' '.join(args)
+        result = perform_snmpwalk(ip, full_oid)
         await ctx.send(f'SNMP Walk Result:\n{result}')
     except Exception as e:
         await ctx.send(f'Error during SNMP Walk:\n{str(e)}')
@@ -96,10 +126,8 @@ def perform_snmpwalk(ip, oid):
     except Exception as e:
         raise Exception(f"Error in SNMP Walk: {str(e)}")
 
-
 @bot.command(name='sobre')
 async def about(ctx):
     await ctx.send(f"Um bot para auxiliar no monitoriamento do zabbix. \n\nQuero mostra os incidentes, hosts e tudo mais, basta apenas você digitar o prefixo '!' junto do comando para eu te responder.")
 
 bot.run(TOKEN)
-
